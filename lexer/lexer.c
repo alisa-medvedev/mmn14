e38e54d759ce8f0c719d7e0c3dd66ba9db2d558a
alisa-medvedev <alisaalisa989@gmail.com>
2023-08-12 18:25:45 +0300

    almost done

--- lexer/lexer.c
+++ lexer/lexer.c
@@ -1,6 +1,5 @@
 
 #include "lexer.h"
-#include "base.h"
 
 #define DATA_MIN_PARAM -2048
 #define DATA_MAX_PARAM 2047
@@ -12,7 +11,7 @@ trie_node instruction_lookup;
 trie_node directive_lookup;
 
 /*define a boolean type*/
-typedef enam booleans {
+typedef enam boolean {
     FALSE = 0, TRUE = 1
 } boolean;
 
@@ -23,7 +22,6 @@ void skip_spaces(char *line) {
     }
 }
 
-/*operand options: I - Integer  L - Label  R - Register*/
 static struct instraction_options {
     const char *inst_name;
     int inst_key;
@@ -50,27 +48,27 @@ static struct instraction_options {
     {"stop", stop, NULL, NULL}
 };
 
-
+/*L - label I - array of integers S - string*/
 static struct diractive_options {
     const char *diractive_name;
     int dir_key;
-    int param_opt;
+    const char dir_param_type;
 }diractive_options[4] = {
-    {"data", dir_data_line, 'I'},
-    {"string", dir_string_line, 'S'},
     {"entry", dir_entry_line, 'L'},
-    {"extern", dir_extern_line, 'L'}
+    {"extern", dir_extern_line, 'L'},
+    {"data", dir_data_line, 'I'},
+    {"string", dir_string_line, 'S'}
 };
 
-static boolean label_parser(char *p1, char *p2, char *token, struct syntex_tree *ast) {
+static boolean label_parser(char *p1, char *p2, char *lbl, struct syntex_tree *ast) {
     int count = 0;
     if(!isalpha(*p1)) {
         strcpy(ast->syntax_error,"The first character of a label is not a letter.");
         return FALSE;
     }
-    *token = *p1;
+    *lbl = *p1;
     p1++;
-    token++;
+    lbl++;
     while(p1 != p2) {
         if(!isalnum(*p1)) {
             strcpy(ast->syntax_error,"Label contains non alpha numeric characters.");
@@ -80,21 +78,88 @@ static boolean label_parser(char *p1, char *p2, char *token, struct syntex_tree 
             strcpy(ast->syntax_error,"Label excedes the max label length.");
             return FALSE;
         }
-        *token = *p1;
+        *lbl = *p1;
         count++;
         p1++;
-        token++;
+        lbl++;
     }
     return TRUE;
 }
 
-static boolean inst_operand_parser(char *p1, char *p2, char *token, struct syntex_tree *ast, struct instraction_options *inst) {
-    char operand_type;
-    int operand_num;
-    int int_operand;
+static boolean dir_operand_parser(char *p1, struct syntex_tree *ast, struct directive_options *dir) {
+    char *s;
+    int *n,i;
+    char str[MAX_STRING_LEN] = {0};
+    s = str;
+    int int_arr[MAX_NUM_ARR_LEN];
+    n = int_arr;
+
+    if(dir->dir_param_type == 'L') {
+        if(ast->label[0] != '\0') {
+            strncpy(ast->label, str, LABEL_LEN); /*label declaration before .entry/.extern have to be ignored*/
+        }
+        p2 = p1;
+        for( ; *p2 != ' ' && *p2 != '\t' && *p2 != '\0'; p2++) {
+            ;
+        }
+        if(label_parser(p1, p2, s, &ast)) {
+            strcpy(ast->label, str);
+            return TRUE;
+        }
+        return FALSE;
+    }
+    else if(dir->dir_param_type == 'I') {
+        do {
+            if(i == MAX_NUM_ARR_LEN) {
+                (ast->syntax_error,"too many operands");
+            }
+            skip_spaces(p1);
+            if(*p1 != '-' && *p1 != '+' && !isdigit(*p1)) {
+                (ast->syntax_error,"invalid operand");
+                return FALSE;
+            }
+            if(!numeric_operand_parser(p, &int_arr[i], DATA_MIN_PARAM, DATA_MAX_PARAM)) {
+                (ast->syntax_error,"invalid operand");
+                return FALSE;
+            }
+            i++;
+            p2 = p1;
+            } while(p2 = strchr(p1, ','));
+            return TRUE;
+    }
+    else {
+        if(*p1 != '"') {
+            strcpy(ast->syntax_error,"invalid start of string directive operand");
+            return FALSE;
+        }
+        p2 = p1 + 1;
+        p2 = strchr(p1, '"');
+        if(!p2) {
+            strcpy(ast->syntax_error,"invalid ending of string directive operand");
+            return FALSE;
+        }
+        for( ; p1 != p2 && s != '\0'; p1++, s++) {
+            *s = *p1;
+        }
+        if(skip_spaces(p1) != '\0') {
+            strcpy(ast->syntax_error,"invalid operand");
+            return FALSE;
+        }
+        strcpy(ast->inst_or_dir.dir_line.dir_info, str);
+        return TRUE;
+    }
+
+}
+
+static boolean inst_line_parser(char *p1, char *p2, struct syntex_tree *ast, struct instraction_options *inst) {
+    int int_op;
+    int reg_num;
+    char label_op[LABEL_LEN]; 
+
     /*check the case of 0 parameters*/
     if(p1 == '\0') {
-        if(inst->source_operand_opt == NULL && inst->onst char dest_operand_opt) {
+        if(inst->source_operand_opt == NULL && inst->dest_operand_opt == NULL) {
+            ast->inst_or_dir.inst_line.inst_opperands_option = {no_op, no_op};
             return TRUE;
         }
         strcpy(ast->syntax_error,"missing parameters");
@@ -103,68 +168,63 @@ static boolean inst_operand_parser(char *p1, char *p2, char *token, struct synte
     p2 = strchr(line, ',');
     /*if ',' found - there should be at least 2 parameters*/
     if(p2) {
-        if(inst->source_operand_opt != NULL) {
+        if(inst->source_operand_opt == NULL) {
             strcpy(ast->syntax_error,"instraction --- recieves only 1 or none parameters");
             return FALSE;
         }
-        operand_num = 2;
+        inst_operand_parser(p1, &int_op, &reg_num, &label_op, ast, inst->source_operand_opt)/*first get source operand*/
     }
-    else {
-        operand_num = 1;
-    }
-    while(operand_num < 3) {
-        operand_type = return_param_type(p1, )
-        switch(*curr) {
-            case 'I':
-                const_op = numeric_operand_parser(p1, INST_PARAM_MIN, INST_PARAM_MAX);
-            case 'L':
-            case 'R':
-                if(*++p1 != 'r') {
-                    strcpy(ast->syntax_error,"Unrecognized parameter");
-                    ast.union_option = ast.syntax_error;
-                    return FALSE;
-                }
-                p1++;
-                if(*p1 > 47 && *p1 < 56)
-            default:
-                return FALSE;
+    return_inst_param_type(p1, &int_op, &reg_num, &label_op ast, inst->dest_operand_opt)/*get dest operand*/
+    return TRUE
+}
+
+static boolean inst_operand_parser(char *p1,char *p2, int *int_op, int *reg_num, char *label_op, struct syntex_tree *ast, struct instraction_options **inst) {
+    char *label_end;
+    if(*p1 == '-' || *p1 == '+' || isdigit(*p1)) {
+        if(numeric_operand_parser(p, int_op, INST_PARAM_MIN, INST_PARAM_MAX)) {
+            return TRUE;
         }
     }
-
+    else if(*p1 == '@') {
+        if(++*p1 != 'r') {
+            return -1;
+        }
+        p1++;
+        if(*p1 > 47 && *p1 < 56) {
+            *reg_num = *p1 - '0';
+            return register_op;
+        }
+        return -1;
+    }
+    else if(isalpha(*p1)) {
+        return label_op;
+    }
+    else {
+        return -1;
+    }
 }
 
 /**/
-static int numeric_operand_parser(char *p, int min, int max) {
-    int num_op, sign;
+static boolean numeric_operand_parser(char *p, int *num_op, int min, int max) {
+    int sign;
     sign = (*p == '-') ? -1 : 1;
-    if(*p == '+' || *p == '-' ) {
-        *token = *p;
+    if(!isdigit(*p)) {
         p++;
-        token++;
     }
-    for(num_op = 0; isdigit(*p); p++){
-        num_op = 10 * num_op + (*p - '0');
-        if((sign == -1 && sign * num_op < min) || (sign == 1 && sign * num_op > max)) {
+    for(*num_op = 0; isdigit(*p); p++){
+        *num_op = 10 * (*num_op) + (*p - '0');
+        if((sign * *num_op < min) || (sign * *num_op > max)) {
             return FALSE;
         }
     }
-}
-
-char return_param_type(char *p) {
-    if(*p == @) {
-        return 'R';
-    }
-    else if(*p == '-' || *p == '+' || isdigit(*p)) {
-        return 'I';
-    }
-    else if(isalpha)
+    return TRUE;
 }
 
 
 struct syntex_tree get_pattern(char *line) {
 
     char *p1,*p2;
-    char token[MAX_STRING_LEN] = {0};
+    char dcl_lbl[LABEL_LEN] = {0};
     struct syntex_tree ast = {0};
     struct instraction_options *inst = NULL;
     struct diractive_options *dir = NULL;
@@ -176,17 +236,16 @@ struct syntex_tree get_pattern(char *line) {
     p2 = strchr(line, ':');
     if(p2) {
         p1 = line;
-        if(!label_parser(p1, p2, token, ast)) {
-            ast.union_option = ast.syntax_err;
+        if(!label_parser(p1, p2, &dcl_lbl, &ast)) {
+            ast.line_type = syntax_error;
             return ast;
         }
-        strcpy(ast.label, token);
-        token[0] = '\0';
+        strcpy(ast.label, dcl_lbl);
         line = p2 + 1;
         skip_spaces(line);
         if(*line == '\0') {
-            strcpy(ast->syntax_error,"Empty line after a label declaration.");
-            ast.line_type = ast.syntax_error;
+            strcpy(ast.syntax_error,"Empty line after a label declaration.");
+            ast.line_type = syntax_error;
             return ast;
         }
     }
@@ -195,31 +254,34 @@ struct syntex_tree get_pattern(char *line) {
         dir = search_trie(directive_lookup, line);
         if(dir == NULL){
             strcpy(ast.syntax_error,"Undefined directory.");
-            ast.union_option = ast.syntax_error;
+            ast.line_type = syntax_error;
             return ast;
         }
+        ast.line_type = dir_line;
         ast.inst_or_dir.dir_line.dir_option = dir->dir_key;
-
-        /*if we will not have syntax errors*/
-        ast.union_option = ast.dir_line;
+        skip_spaces(line);
+        if(!dir_operand_parser(line, &ast, dir)) {
+            ast.line_type = syntax_error;
+            return ast;
+        }
     }
     else{
         inst = search_trie(instruction_lookup, line);
         if(inst == NULL){
             strcpy(ast.syntax_error,"Undefined instraction.");
-            ast.union_option = ast.syntax_error;
+            ast.line_type = syntax_err;
             return ast;
         }
-        ast.inst_or_dir.inst_line.inst_name = inst->inst_key;
-        line = line + (strlen(inst->inst_name[0]) - 1);
+        ast->inst_or_dir.inst_line.inst_name = inst->inst_key;
         skip_spaces(line);
         p1 = line;
         p2 = p1;
-        inst_operand_parser(p1, p2, token, ast, inst);
-
-
-        /*if we will not have syntax errors*/
-        ast.union_option = ast.inst_line;
+        if(!inst_operand_parser(p1, p2, token, ast, inst)) {
+            ast.line_type = syntax_err;
+            return ast;
+        }
+        ast->line_type = inst_line;
+        return ast;
     }
 
 }
